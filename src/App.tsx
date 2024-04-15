7f00581f8367085c5eac5f40c3713c1a099175b1
lordrichard6 <paulolopesreizinho@gmail.com>
2024-04-15 20:19:35 +0200

    task:create post form

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Toaster } from "@/components/ui/toaster"
 
 import './globals.css';
-import { Home } from './_root/pages';
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
 import AuthLayout from './_auth/AuthLayout';
@@ -21,6 +21,14 @@ const App = () => {
                 {/* privat routes */}
                 <Route element={<RootLayout />}>
                     <Route index element={<Home />} />
+                    <Route path="/explore" element={<Explore />} />
+                    <Route path="/saved" element={<Saved />} />
+                    <Route path="/all-users" element={<AllUsers />} />
+                    <Route path="/create-post" element={<CreatePost />} />
+                    <Route path="/update-post/:id" element={<EditPost />} />
+                    <Route path="/posts/:id" element={<PostDetails />} />
+                    <Route path="/profile/:id/*" element={<Profile />} />
+                    <Route path="/update-profile/:id" element={<UpdateProfile />} />
                 </Route>
             </Routes>
 
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,29 +1,59 @@
 import {
   // useQuery,
   useMutation,
+  useQueryClient,
   // useQueryClient,
   // useInfiniteQuery,
 } from "@tanstack/react-query";
-import { createUserAccount, SignInAccount, signOutAccount } from "../appwrite/api";
-import { INewUser } from "@/types";
+import {
+  createPost,
+  createUserAccount,
+  SignInAccount,
+  signOutAccount,
+  updatePost,
+} from "../appwrite/api";
+import { INewPost, INewUser, IUpdatePost } from "@/types";
+import { QUERY_KEYS } from "./queryKeys";
 
 export const useCreateUserAccount = () => {
-    return useMutation({
-        mutationFn: (user: INewUser) => createUserAccount(user)
-    })
-}
+  return useMutation({
+    mutationFn: (user: INewUser) => createUserAccount(user),
+  });
+};
 
 export const useSignInAccount = () => {
   return useMutation({
-      mutationFn: (user: {
-        email: string;
-        password: string;
-      }) => SignInAccount(user)
-  })
-}
+    mutationFn: (user: { email: string; password: string }) =>
+      SignInAccount(user),
+  });
+};
 
 export const useSignOutAccount = () => {
   return useMutation({
     mutationFn: signOutAccount,
+  });
+};
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    },
+  });
+};
+
+export const useUpdatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: IUpdatePost) => updatePost(post),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id],
+      });
+    },
   });
 };
